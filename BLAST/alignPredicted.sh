d601421e9628593119ca6bd481ad2cd65420b223
taylorjohnsonm95 <74315746+taylorjohnsonm95@users.noreply.github.com>
2021-08-05 12:09:56 -0400

    Update alignPredicted.sh

--- BLAST/alignPredicted.sh
+++ BLAST/alignPredicted.sh
@@ -4,5 +4,5 @@
 
 blastp -query Trinity.fasta.transdecoder.pep \
     -db swissprot -max_target_seqs 1 \
-    -outfmt "6 qseqid sacc qlen slen length nident pident evalue < 1e-10 stitle" \
+    -outfmt 6 \
     1>alignPredicted.txt 2>alignPredicted.err &
